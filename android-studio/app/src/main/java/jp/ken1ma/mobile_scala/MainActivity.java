f2de734a248e4b79b7ac5b60e729b69faf4ac5c4
Kenichi Masuko <>
2020-04-06 17:28:48 +0900

    Android Studio 3.6.2: Start a new Project   Project Template: Empty Activity   Language: Java   Minimum SDK: API 21 (5.0 Lollipop)   Use legacy android.support libraries: No
    Note that `Reimport Gradle Project` causes an error because of a bug
    https://youtrack.jetbrains.com/issue/IDEA-225359#focus=streamItem-27-4021239.0-0
    Removing ExampleInstrumentedTest avoids the error.
    
    Note that on a fresh checkout an error occurs
    `Unsupported Modules Detected: Compilation is not supported for following modules: android-studio. Unfortunately you can't have non-Gradle Java modules and Android-Gradle modules in one project.`
    `Close project`, delete `idea`, then `Invalidate Caches & Restart` fixes the error.

---
+++ android-studio/app/src/main/java/jp/ken1ma/mobile_scala/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ken1ma.mobile_scala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
